bba01d2c5b47c69195ddee59525a79807aad499d
reem <reemfarouq031@gmail.com>
2025-06-24 15:54:35 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import React, { Component } from 'react'
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import './assets/light.png'
+import Hello from './components/Hello'
+import About from './components/about'
+import Object from './components/Object'
+import AboutMe from './components/AboutMe'
+//port AboutMe from './components/AboutMe'
 
-export class App extends Component {
-  render() {
-    return (
-      <div>App</div>
-    )
-  }
+import Footer from './components/Footer'
+function App() {
+
+
+  return (
+    <main className='overflow-x-hidden'>
+       
+        <Navbar />
+        <Hero />
+        <Hello />
+        <About />
+        <Object />
+        <AboutMe />
+        
+        <Footer />
+    </main>
+  )
 }
 
 export default App
